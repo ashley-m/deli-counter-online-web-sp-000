24d06d167317927a9b81ea212347590d3cadbed5
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 02:42:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def take_a_number(array, name)
+  array<<name
+  puts "Welcome, #{name}. You are number #{array.index(name)+1}"
+end
+
+def line
+end
+
+def now_serving
+end
