38a8896d52b2912aab4bd8cc894032618bf62220
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 03:02:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,21 @@
 # Write your code here.
 def take_a_number(array, name)
   array<<name
-  puts "Welcome, #{name}. You are number #{array.index(name)+1}"
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
 end
 
-def line
+def line(array)
+  if array.size==0
+    puts "The line is empty."
+  else
+    puts "The line is currently: "
+    
 end
 
 def now_serving
+end
+
+def convert(array)
+  list = []
+  array.each {|x| list<<"#{array.index(x)}. #{x}"}
 end
