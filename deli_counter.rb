608da394c3fba66e8d03ed348cf9e3541891ebd0
Ashley Miller <asm2fe@virginia.edu>
2019-03-28 03:09:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
   if array.size==0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{convert(list).join(" ")}"
+    puts "The line is currently: #{convert(array).join(" ")}"
   end
 
 end
